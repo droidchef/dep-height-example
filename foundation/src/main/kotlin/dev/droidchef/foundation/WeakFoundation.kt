3243f628046bcbb615c36c6b57442833a359412f
Ishan Khanna <ishan1604@gmail.com>
2022-11-21 18:07:03 -0800

    Changed Foundation

--- foundation/src/main/kotlin/dev/droidchef/foundation/WeakFoundation.kt
+++ foundation/src/main/kotlin/dev/droidchef/foundation/WeakFoundation.kt
@@ -2,6 +2,7 @@ package dev.droidchef.foundation
 
 class WeakFoundation : Foundation {
     override fun getStrength(): Int {
+        println("This is a weak foundation!!")
         return 50
     }
 }
