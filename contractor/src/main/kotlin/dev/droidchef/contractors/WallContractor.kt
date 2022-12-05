7a0e9ffdcb14ecb72b5f055e38e46ac8cf2a81bd
Ishan Khanna <ishan1604@gmail.com>
2022-12-04 22:09:10 -0800

    :contractor modified

--- contractor/src/main/kotlin/dev/droidchef/contractors/WallContractor.kt
+++ contractor/src/main/kotlin/dev/droidchef/contractors/WallContractor.kt
@@ -4,9 +4,14 @@ import dev.droidchef.wall.Wall
 
 class WallContractor {
 
+    init {
+        println("Hello I am the Wall Contractor")
+    }
+
     companion object {
 
         fun buildDryWall(height: Double, width: Double, depth: Double): Wall {
+            println("Hello I am the Wall Contractor")
             return DryWall(h = height, w = width, d = depth)
         }
 
